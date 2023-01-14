8110532e1c768be34d8e710ccc885661850de8ab
Huuuunee <s21024@gsm.hs.kr>
2023-01-14 17:29:01 +0900

    :tada: 시작

---
+++ src/main/kotlin/com/example/demo/domain/auth/presentation/controller/AuthController.kt
@@ -0,0 +1,10 @@
+package com.example.demo.domain.auth.presentation.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class AuthController {
+
+}
